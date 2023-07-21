f69fe8de84b853c0935a06831708ba38b21efe31
mateovg <mvela092@fiu.edu>
2023-07-21 08:03:00 -0400

    65%!!

--- test.py
+++
@@ -1,9 +0,0 @@
-from sklearn.svm import LinearSVC
-from sklearn.datasets import load_iris
-from sklearn.feature_selection import SelectFromModel
-X, y = load_iris(return_X_y=True)
-X.shape
-lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X, y)
-model = SelectFromModel(lsvc, prefit=True)
-X_new = model.transform(X)
-print(X_new.shape)
